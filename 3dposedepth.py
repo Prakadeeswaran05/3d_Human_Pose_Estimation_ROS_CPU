776ef41e0ad8a14f1208a0084b88b28f7c5f7038
Prakadeeswaran Manivannan <35860872+Prakadeeswaran05@users.noreply.github.com>
2021-09-18 10:43:10 +0530

    Improved code for easier readability

--- 3dposedepth.py
+++ 3dposedepth.py
@@ -20,9 +20,7 @@ from geometry_msgs.msg import Point
 import struct
 import message_filters as mf
 
-mpPose=mp.solutions.pose
-pose=mpPose.Pose()
-mpDraw=mp.solutions.drawing_utils
+
  
     
 class pose3d_estim:
@@ -30,7 +28,9 @@ class pose3d_estim:
         self.rgb_image_sub = mf.Subscriber("/camera/color/image_raw", Image)
         self.depth_image_sub = mf.Subscriber("/camera/depth/image_raw",Image)
         self.camera_info_sub = mf.Subscriber("/camera/depth/camera_info",CameraInfo)
-        
+        self.mpPose=mp.solutions.pose
+        self.pose=self.mpPose.Pose()
+        self.mpDraw=mp.solutions.drawing_utils
         ts = mf.ApproximateTimeSynchronizer([self.rgb_image_sub, self.depth_image_sub,self.camera_info_sub], queue_size=10, slop=0.1)
         ts.registerCallback(self.image_callback)
         self.marker_pub=rospy.Publisher("marker_test", MarkerArray, queue_size=10)
@@ -51,9 +51,9 @@ class pose3d_estim:
         '''   
         pts=[]
         imgRGB=cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
-        results=pose.process(imgRGB)
+        results=self.pose.process(imgRGB)
         if results.pose_landmarks:
-            mpDraw.draw_landmarks(image,results.pose_landmarks,mpPose.POSE_CONNECTIONS)
+            self.mpDraw.draw_landmarks(image,results.pose_landmarks,self.mpPose.POSE_CONNECTIONS)
             for id,lm in enumerate(results.pose_landmarks.landmark):
                 h,w,c=image.shape
                 imgx,imgy=int(lm.x*w),int(lm.y*h)
@@ -61,7 +61,7 @@ class pose3d_estim:
                 pts.append((imgx,imgy)) 
         return pts,image
 
-    def create_line_list(self,depth_arr):
+    def create_line_list(self,depth_arr,rpts):
         """ Creates linelist marker in kinect_frame. """
 
         '''
@@ -69,12 +69,15 @@ class pose3d_estim:
         '''
 
         try:
-            body=[['shoulder_line',[self.rpts[11],self.rpts[12]]],['waist_line',[self.rpts[23],self.rpts[24]]],['left_shoulder_waist',[self.rpts[11],self.rpts[23]]],['right_shoulder_waist',[self.rpts[12],self.rpts[24]]],['right_thigh',[self.rpts[24],self.rpts[26]]],['left_thigh',[self.rpts[23],self.rpts[25]]],['right_leg',[self.rpts[26],self.rpts[28]]],['left_leg',[self.rpts[25],self.rpts[27]]],['right_forearm',[self.rpts[14],self.rpts[16]]],['left_forearm',[self.rpts[13],self.rpts[15]]],['right_bicep',[self.rpts[12],self.rpts[14]]],['left_bicep',[self.rpts[11],self.rpts[13]]]]
+            body=[['shoulder_line',[rpts[11],rpts[12]]],['waist_line',[rpts[23],rpts[24]]],['left_shoulder_waist',[rpts[11],rpts[23]]],
+            ['right_shoulder_waist',[rpts[12],rpts[24]]],['right_thigh',[rpts[24],rpts[26]]],['left_thigh',[rpts[23],rpts[25]]],
+            ['right_leg',[rpts[26],rpts[28]]],['left_leg',[rpts[25],rpts[27]]],['right_forearm',[rpts[14],rpts[16]]],
+            ['left_forearm',[rpts[13],rpts[15]]],['right_bicep',[rpts[12],rpts[14]]],['left_bicep',[rpts[11],rpts[13]]]]
             self.linelist.points=[]
             self.linelist.header.frame_id = "kinect_frame"
             self.linelist.header.stamp = rospy.Time.now()
             self.linelist.type = Marker.LINE_LIST
-        
+            self.linelist.pose.orientation.w = 1.0
             self.linelist.id=1
             self.linelist.action = Marker.ADD 
             self. linelist.scale.x = 0.05
@@ -97,7 +100,7 @@ class pose3d_estim:
                 
         except:
             pass         
-    def create_spheres(self,depth_arr):
+    def create_spheres(self,depth_arr,rpts):
         """ Creates sphere list marker in kinect_frame. """
 
 
@@ -107,7 +110,7 @@ class pose3d_estim:
 
         try:
             #points=[nose,left_wrist,right,wrist,left_ankle,right ankle]
-            points=[self.rpts[0],self.rpts[15],self.rpts[16],self.rpts[27],self.rpts[28]]
+            points=[rpts[0],rpts[15],rpts[16],rpts[27],rpts[28]]
             self.spheres.points=[]
             self.spheres.header.frame_id = "kinect_frame"
             self.spheres.header.stamp= rospy.Time.now()
@@ -117,6 +120,7 @@ class pose3d_estim:
                 
                     #points
             self.spheres.type = Marker.SPHERE_LIST
+            self.spheres.pose.orientation.w = 1.0
             self.spheres.color.r = 1.0
             self.spheres.color.a = 1.0
                     
@@ -158,15 +162,15 @@ class pose3d_estim:
 
     def image_callback(self,rgb_data,depth_data,camera_data):
         try:
-            self.img = self.bridge.imgmsg_to_cv2(rgb_data,"bgr8")
-            self.d_img= self.bridge.imgmsg_to_cv2(depth_data,"passthrough")
-            print(self.d_img.shape,self.img.shape)
-            self.rpts,self.rimg=self.pose_2d_pts(self.img)
-            depth_arr=np.array(self.d_img)
+            img = self.bridge.imgmsg_to_cv2(rgb_data,"bgr8")
+            d_img= self.bridge.imgmsg_to_cv2(depth_data,"passthrough")
+            print(d_img.shape,img.shape)
+            rpts,rimg=self.pose_2d_pts(img)
+            depth_arr=np.array(d_img)
             
             self.cam_intrin=list(camera_data.K)
-            self.create_line_list(depth_arr)
-            self.create_spheres(depth_arr)
+            self.create_line_list(depth_arr,rpts)
+            self.create_spheres(depth_arr,rpts)
             self.marker_array.markers.extend([self.spheres,self.linelist])
             self.marker_pub.publish(self.marker_array)
 
@@ -174,7 +178,7 @@ class pose3d_estim:
         except CvBridgeError as e:
             print(e)
            
-        cv2.imshow('image',self.rimg)
+        cv2.imshow('image',rimg)
         cv2.waitKey(1)
 
    
